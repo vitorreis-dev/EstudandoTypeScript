b435cd8519976228e00cadb20d6bc20945f1590e
vitorreis-dev <vitordosreisvitor@gmail.com>
2026-08-18 20:33:07 -0300

    Adiciona projeto da aula 3

--- aula-3/src/App.tsx
+++ aula-3/src/App.tsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css"
+import { useState } from "react";
 
-function App() {
+export default function App() {
 
-  const [red,setRed] = useState([])
-  const [green,setGreen] = useState([])
-  const [blue,setBlue] = useState([])
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const rgb = `rgb(${red}, ${green}, ${blue})`
 
   return (
-    <section>
-      <label htmlFor="red">R</label>
-      <input type="number" name="" id="red" min={0} max={255} />
-    </section>
-  )
-}
+    <>
+    <section className="colors-section">
+      <div className="color-div corzinha-r">
+        <label htmlFor="red">R</label>
+        <input id="red" type="number" min={0} max={255} value={red} onChange={(event) => setRed(Number(event.target.value))}
+        />
+      </div>
 
-export default App
+      <div className="color-div corzinha-g">
+        <label htmlFor="green">G</label>
+        <input id="green" type="number" min={0} max={255} value={green} onChange={(event) => setGreen(Number(event.target.value))}
+        />
+      </div>
+
+      <div className="color-div corzinha-b">
+        <label htmlFor="blue">B</label>
+        <input id="blue" type="number" min={0} max={255} value={blue} onChange={(event) => setBlue(Number(event.target.value))}
+        />
+      </div>
+    </section>
+    <article className="colorOutput-article" style={{backgroundColor:rgb}}>
+       <p>RGB({red},{blue},{green})</p>
+    </article>
+    </>
+  );
+}
